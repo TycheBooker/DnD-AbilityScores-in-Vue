d309a06129d45fba22c581405fe34d088e6a8ab4
Tihana Bihler <tihana.bihler@broj42.com>
2017-10-26 16:16:20 +0200

    new cli template

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  extends: ['eslint:recommended'],
+  // add your custom rules here
+  rules: {
+    // allow debugger and console during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+  }
+}
